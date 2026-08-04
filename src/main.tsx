aeafaf511b1f0cd56ac157ba03bd0b0205ba1813
jacoblbagent <jacoblbagent@users.noreply.github.com>
2026-08-04 13:29:44 -0400

    Add BrowserRouter basename for subpath deployment

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/broughton-web-development">
       <App />
     </BrowserRouter>
   </StrictMode>,
